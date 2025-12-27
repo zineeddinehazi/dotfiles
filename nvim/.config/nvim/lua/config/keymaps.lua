8a28ddd932dcfabde466b397b9e8ad39afeeab7a
zineeddinehazi <z.hazi.igee@univ-boumerdes.dz>
2025-12-27 02:12:19 +0100

    remove opts var from keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,10 @@
 local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
 
 -- Panel/window navigation (splits)
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
+map("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+map("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+map("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+map("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- Code actions / hover docs
 vim.api.nvim_create_autocmd("LspAttach", {
